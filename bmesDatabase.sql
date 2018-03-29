6eedeb5373f320748d8f2881270b0203c76223c4
prestaylor51 <prestaylor51@gmail.com>
2018-03-29 17:30:19 -0600

    modified bmesdatabase

--- bmesDatabase.sql
+++ bmesDatabase.sql
@@ -3,7 +3,7 @@
 -- CREATE DATABASE bmes
 
 -- Drop table every time we run
-Drop Table gender, student CASCADE;
+Drop Table gender, student, employee, visit CASCADE;
 
 -- Create gender table
 CREATE TABLE gender(
@@ -25,12 +25,25 @@ id 				SERIAL   		PRIMARY KEY
 ,name_first		VARCHAR(100)
 ,name_last		VARCHAR(100)
 ,birthday		DATE
-,byui_id		INT
+,byui_id		   INT
 ,gender			INT      		REFERENCES gender(id)	
+);
+
+CREATE TABLE employee (
+id 				SERIAL			PRIMARY KEY
+,name_first 	VARCHAR(100)
+,name_last		VARCHAR(100)
 );
 
 -- Create visit table
 CREATE TABLE visit(
 id 				SERIAL 			PRIMARY KEY
+,visit_date		DATE
 ,student_id		INT 			REFERENCES student(id)
-,);
+,employee_id	INT 			REFERENCES employee(id) 
+,weight			DECIMAL	 
+,height			INT	 
+,pulse			INT 
+,systolic		INT
+,diastolic		INT
+);
